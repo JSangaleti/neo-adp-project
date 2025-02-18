c500de95cbdb75b11d222d8b19c2b02e354353e5
JSangaleti <julianosangaleti@alunos.utfpr.edu.br>
2025-02-18 15:00:09 -0300

    CRUD Profissional de Saúde + estruturação do controller Criado CRUD de PDS (ainda com algumas deficiências a serem melhoradas, como obrigatoriedade de campos que não são obrigatórios) e foi realizada algumas implementações básicas que preparam para interligar os CRUDS à base de dados com a interface de Admin do Django.

--- NeoADPproject/PEWS/admin.py
+++ NeoADPproject/PEWS/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . models import profissional_de_saude
 # Register your models here.
 
+class PDSadmin(admin.ModelAdmin):
+    list_display = ('nome', 'crm', 'coren')
+
+admin.site.register(profissional_de_saude, PDSadmin)
--- NeoADPproject/PEWS/models.py
+++ NeoADPproject/PEWS/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class profissional_de_saude(models.Model):
+    id = models.IntegerField(primary_key=1, auto_created=1)
+    crm = models.CharField(max_length=16, unique=1, null=1)
+    coren = models.CharField(max_length=16, unique=1, null=1)
+    nome = models.CharField(max_length=64)
+    telefone = models.CharField(max_length=16)
+    senha = models.CharField(max_length=16)
