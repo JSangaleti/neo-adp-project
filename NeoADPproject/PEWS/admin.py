14e94339f2bafa9dfee4f6d409a0b59794f0bbbc
JSangaleti <julianosangaleti@alunos.utfpr.edu.br>
2025-02-20 16:58:47 -0300

    Criação do CRUD Criança Criação do modelo e CRUD na área de admin para paciente criança.
    Closes #3

--- NeoADPproject/PEWS/admin.py
+++ NeoADPproject/PEWS/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from . models import profissional_de_saude, paciente_crianca
 # Register your models here.
 
+class PDSadmin(admin.ModelAdmin):
+    list_display = ('nome', 'crm', 'coren')
+    
+class PCadmin(admin.ModelAdmin):
+    list_display = ('nome', 'data_de_nascimento')
+
+admin.site.register(profissional_de_saude, PDSadmin)
+admin.site.register(paciente_crianca, PCadmin)
