c21666735c98fda5ccb5fb1904ea0ebf373add43
JSangaleti <julianosangaleti@alunos.utfpr.edu.br>
2025-02-25 15:20:31 -0300

    CRUD Responsável Criação do CRUD Responsável e reparo de alguns detalhes na estrutura para conexão com o banco de dados.
    closes #2

--- NeoADPproject/PEWS/admin.py
+++ NeoADPproject/PEWS/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from . models import profissional_de_saude, paciente_crianca
+from . models import paciente_crianca, profissional_de_saude, responsavel_crianca
 # Register your models here.
 
 class PDSadmin(admin.ModelAdmin):
@@ -7,6 +7,10 @@ class PDSadmin(admin.ModelAdmin):
     
 class PCadmin(admin.ModelAdmin):
     list_display = ('nome', 'data_de_nascimento')
+    
+class RCadmin(admin.ModelAdmin):
+    list_display = ('nome', 'telefone', 'cpf')
 
 admin.site.register(profissional_de_saude, PDSadmin)
 admin.site.register(paciente_crianca, PCadmin)
+admin.site.register(responsavel_crianca, RCadmin)
