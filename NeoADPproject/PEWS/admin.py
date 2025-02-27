4b0b14c98491c3202de916eaab18c7e957e8c066
JSangaleti <julianosangaleti@alunos.utfpr.edu.br>
2025-02-27 20:50:42 -0300

    CRUD Internação Foi criado o CRUD Internação e foi criado também, em complemento, uma entidade para registrar o histórico de pontuações de um paciente em determinada internação, com data e horário de registro
    Fixes #4

--- NeoADPproject/PEWS/admin.py
+++ NeoADPproject/PEWS/admin.py
@@ -1,16 +1,31 @@
 from django.contrib import admin
-from . models import paciente_crianca, profissional_de_saude, responsavel_crianca
+from .models import (
+    paciente_crianca,
+    profissional_de_saude,
+    responsavel_crianca,
+    internacao,
+)
+
 # Register your models here.
 
+
 class PDSadmin(admin.ModelAdmin):
-    list_display = ('nome', 'crm', 'coren')
-    
+    list_display = ("nome", "crm", "coren")
+
+
 class PCadmin(admin.ModelAdmin):
-    list_display = ('nome', 'data_de_nascimento')
-    
+    list_display = ("nome", "data_de_nascimento")
+
+
 class RCadmin(admin.ModelAdmin):
-    list_display = ('nome', 'telefone', 'cpf')
+    list_display = ("nome", "telefone", "cpf")
+
+
+class internacao_admin(admin.ModelAdmin):
+    list_display = ("leito", "paciente_crianca_matricula")
+
 
 admin.site.register(profissional_de_saude, PDSadmin)
 admin.site.register(paciente_crianca, PCadmin)
 admin.site.register(responsavel_crianca, RCadmin)
+admin.site.register(internacao, internacao_admin)
--- NeoADPproject/PEWS/models.py
+++ NeoADPproject/PEWS/models.py
@@ -15,9 +15,31 @@ class paciente_crianca(models.Model):
     matricula = models.IntegerField()
     nome = models.CharField(max_length=32)
     data_de_nascimento = models.DateField()
-    
-    
+
+
 class responsavel_crianca(models.Model):
     cpf = models.CharField(primary_key=1, max_length=16)
     nome = models.CharField(max_length=64)
     telefone = models.CharField(max_length=16)
+
+
+class internacao(models.Model):
+    protocolo = models.IntegerField(primary_key=1)
+    paciente_crianca_matricula = models.IntegerField(
+        models.ForeignKey(
+            "PEWS.paciente_crianca", verbose_name=(""), on_delete=models.CASCADE
+        )
+    )
+    leito = models.CharField(max_length=16)
+    dih = models.FloatField()
+
+
+class relatorio_pews(models.Model):
+    relatorio_id = models.IntegerField(primary_key=1, auto_created=1)
+    internacao_protocolo = models.IntegerField(
+        models.ForeignKey(
+            "PEWS.internacao", verbose_name=(""), on_delete=models.CASCADE
+        )
+    )
+    pontuacao_pews = models.IntegerField()
+    horario = models.DateTimeField()
